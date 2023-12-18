b5e8040b200a019ae2286154fc6cb08867bd9e9a
Kushankpatil-06 <143101646+Kushankpatil-06@users.noreply.github.com>
2023-12-18 15:26:00 +0000

    done

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accountID = 7487349
+let accountEmail = "kjhfksf@gmial.com"
+var accountPassword = "3232323"
+
+//preder not to use var because of the functional and block scope
+//wihtour defining variables can be cretaed
+console.table([accountEmail,accountID,accountPassword]);
