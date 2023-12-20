78d56787a670f2b199e4825f62a3637ab81da659
Kushankpatil-06 <143101646+Kushankpatil-06@users.noreply.github.com>
2023-12-20 16:34:00 +0000

    dt

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+///conversion of the data types
+//"33" => number
+//"33abc" => Nan 
+//true = 1;
+/////////////////////OPERATIONS////
+let value = 3
+console.log("1"+2);
+console.log(1 + "2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+//comparison operator converts null to a number treating it as a 0
+//=== checks the datatype also
+
+////SUMMARY
+//primitive
+//7 TYPES: string number boolean null undefined symbol(to make unique) Bigint
+//reference type: non primitive
+//reference goes : array objects functions
+
